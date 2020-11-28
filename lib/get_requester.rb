610bdd7b39bb192cdc158bc6f1fdb0b960afd305
Wesley <wes.beck21@outlook.com>
2020-11-27 17:06:47 -0800

    commit tree 8bd97779a9cc73aa1e824bcba2cf5b90962c3b64

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require 'json'
+require 'open-uri'
+require 'net/http'
+
+class GetRequester
+
+attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        JSON.parse(self.get_response_body)
+        # use get_response_body method to get data and parse from json
+    end
+end
